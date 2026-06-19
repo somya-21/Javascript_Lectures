a0c542503c5f36b0cb706c2055470ef7cc99af68
somya-21 <123484441+somya-21@users.noreply.github.com>
2026-06-19 07:54:06 +0000

    Solve Error

--- Express/1Practice_project_navigation/app.js
+++ Express/1Practice_project_navigation/app.js
@@ -1,53 +1,45 @@
 const http=require('http')
-
 const server=http.createServer((req,res)=>
 {
-    res.setHeader('Content-Type','text/html')
-//  if(req.url==="/home")
-// {
-//     res.write(`<h1>welcome to Home</h1>`)
-//         return res.end()
+res.setHeader('Content-type','text/html')
+if(req.url==="/home")
+{
+    res.write(`<h1>Welcome to Home</h1>`)
+    return res.end()
+}
+else if(req.url==="/men")
+{
+    res.write(`<h1>Welcome to Men</h1>`)
+    return res.end()
+}
 
-// }
-// else if(req.url==="/men")
-// {
-//     res.write(`<h1>welcome to men</h1>`)
-//         return res.end()
+else if(req.url==="/women")
+{
+    res.write(`<h1>Welcome to Women</h1>`)
+    return res.end()
+}
+else if(req.url==="/kids")
+{
+    res.write(`<h1>Welcome to kids</h1>`)
+    return res.end()
+}
+else if(req.url==="/cart")
+{
+    res.write(`<h1>Welcome to Cart</h1>`)
+    return res.end()
+}
 
-// }
-// else if(req.url==="/women")
-// {
-//     res.write(`<h1>welcome to women</h1>`)
-//         return res.end()
-
-// }
-// else if(req.url==="/cart")
-// {
-//     res.write(`<h1>welcome to cart</h1>`)
-//         return res.end()
-
-// }
-//  else if(req.url==="/kids")
-// {
-//     res.write(`<h1>welcome to kid</h1>`)
-//         return res.end()
-
-// }
-
-
-   
-    res.write(
-       ` <a href="/home">home</a><br>
-                <a href="/men">men</a><br>
-                        <a href="/women">women</a><br>
-                                <a href="/kids">kids</a><br>
-                                        <a href="/cart">cart</a>`
-    )
-  return  res.end()
+res.write(`<ul>
+    <li><a href="/home">Home</a></li>
+     <li><a href="/men">men</a></li>
+      <li><a href="/women">women</a></li>
+       <li><a href="/kids">kids</a></li>
+        <li><a href="/cart">cart</a></li>
+</ul>`)
+res.end()
 })
 
 const port=3000
-server.listen((port),()=>
-{
-    console.log(`Server runig on http://localhost${port}`)
+server.listen(port,()=>{
+    console.log(`server`)
 })
